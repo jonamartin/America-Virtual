ebbf9b0ae612a276f55863b23193d017364a0182
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-12-11 02:52:24 -0300

    Initial commit

---
+++ America Virtual/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent {
+  public formVisible: boolean = false;
+
+  toggleForm() {
+    this.formVisible = this.formVisible ? false : true;
+    console.log(this.formVisible);
+  }
+
+}
